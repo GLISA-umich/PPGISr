5914c6a1baf687393bb0b1a4abab101adbc679ac
Thomas Estabrook <tgestab@umich.edu>
2023-01-12 15:25:12 -0800

    Added basemap name parameter and eliminated superfluous reproject code.

--- R/run_app.R
+++ R/run_app.R
@@ -21,11 +21,9 @@
 #'
 #'
 run_app <- function(
-  editable_map = PPGISr::duluthEditablemap %>%
-    dplyr::mutate(PPGIS_CODE = as.character(dplyr::row_number()),SELECTED = NA) %>%
-    dplyr::select(PPGIS_CODE, SELECTED, geometry) %>% ## everything()
-    sf::st_transform(4326),
+  editable_map = PPGISr::duluthEditablemap,
   base_map = PPGISr::duluthBasemap,
+  basemap_name = 'Reference Basemap',
   mapping_categories = c("High Density Development", "Street Trees", "Infrastructure Need"),
   mapping_colors = c("#880015", "#22b14c", "#00a2e8"),
   editable_map_info_icon_message = "Click the circle to left to choose mapping categories you want to add to the map. Click the map to indicate these preferences",
@@ -46,11 +44,9 @@ run_app <- function(
       uiPattern = uiPattern
     ),
     golem_opts = list(
-    editable_map = editable_map %>%
-      dplyr::mutate(PPGIS_CODE = as.character(dplyr::row_number()),SELECTED = NA) %>%
-      dplyr::select(PPGIS_CODE, SELECTED, geometry) %>% ## everything()
-      sf::st_transform(4326),
+    editable_map = editable_map,
     base_map = base_map,
+    basemap_name = basemap_name,
     mapping_categories = stats::setNames(c(NA, 1:length(mapping_categories)), c("No Category", mapping_categories)),
     mapping_colors = mapping_colors,
     editable_map_info_icon_message = editable_map_info_icon_message,
