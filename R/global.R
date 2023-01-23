c3ebf39f1400a57752df206d0f808613a031c570
Nathan Fox <nf2g13@soton.ac.uk>
2023-01-23 13:19:24 -0500

    - fixing bugs

---
+++ R/global.R
@@ -0,0 +1,12 @@
+#' @noRd
+#'
+utils::globalVariables(c("PPGIS_CODE",
+                         "SELECTED",
+                         "geometry",
+                         "user_basemap",
+                         "basemap_groups",
+                         "basemap_type",
+                         "base_map_bounds",
+                         "bmap_fields",
+                         ".",
+                         "values"))
